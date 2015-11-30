db623e8071af68ac2f8f34aebafc897e857049e9
Robin Karlsson <Robin@ancon.se>
2015-11-30 17:42:38 +0100

    Start
    Start of the repo

---
+++ FastDL/FileManager.cs
@@ -0,0 +1,124 @@
+﻿using ICSharpCode.SharpZipLib.BZip2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDL
+{
+    public class File
+    {
+        public File(string fullpath)
+        {
+            Path = new List<string>();
+
+            foreach (string folder in System.IO.Path.GetDirectoryName(fullpath).Split('\\'))
+            {
+                Path.Add(folder);
+            }
+
+            Name = System.IO.Path.GetFileNameWithoutExtension(fullpath);
+            Ext = System.IO.Path.GetExtension(fullpath);
+
+            //Console.Out.WriteLine(GetFullPath());
+        }
+
+        public string GetFullPath()
+        {
+            string full = "";
+
+            foreach (string folder in Path)
+            {
+                full += folder + "\\";
+            }
+
+            return full + Name + Ext;          
+        }
+
+        public bool IsWithinDirectory(string directory)
+        {
+            directory = directory.ToLower();
+
+            foreach (string folder in Path)
+            {
+                if (folder.ToLower() == directory)
+                    return true;
+            }
+
+            return false;
+        }
+        public List<string> Path {get;set;}
+        public string Name { get; set; }
+        public string Ext { get; set; }
+    }
+    public class FileManager
+    {
+        private List<File> _files;
+
+        public List<File> Files { get { return _files; } }
+
+        public FileManager(string path)
+        {
+            _files = new List<File>();
+            DoFolder(path);
+        }
+
+        private void DoFolder(string path)
+        {
+            foreach (string file in System.IO.Directory.GetFiles(path))
+            {
+                _files.Add(new File(file));
+            }
+
+            foreach (string file in System.IO.Directory.GetDirectories(path))
+            {
+                DoFolder(file);
+            }
+        }
+
+        public void Copy(string dst, string src)
+        {
+            string folder = System.IO.Path.GetDirectoryName(dst);
+            if (!System.IO.Directory.Exists(folder))
+            {
+                System.IO.Directory.CreateDirectory(folder);
+            }
+
+            if (System.IO.File.Exists(dst))
+            {
+                System.IO.File.Delete(dst);
+            }
+
+            System.IO.File.Copy(src, dst);
+        }
+
+        public void Compress(string target)
+        {
+            FileInfo fileToBeZipped = new FileInfo(target);
+            FileInfo zipFileName = new FileInfo(string.Concat(fileToBeZipped.FullName, ".bz2"));
+
+            if (System.IO.File.Exists(zipFileName.FullName))
+            {
+                System.IO.File.Delete(zipFileName.FullName);
+            }
+
+            using (FileStream fileToBeZippedAsStream = fileToBeZipped.OpenRead())
+            {
+                using (FileStream zipTargetAsStream = zipFileName.Create())
+                {
+                    try
+                    {
+                        BZip2.Compress(fileToBeZippedAsStream, zipTargetAsStream, true, 4096);
+                        Console.Out.WriteLine("Compressed: " + zipFileName.FullName);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FastDL/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDL
+{
+    class Program
+    {
+
+        static public bool Ignore(string ext)
+        {
+            if (string.IsNullOrEmpty(ext))
+                return true;
+
+            ext = ext.ToLower();
+
+            switch(ext)
+            {
+                case ".lua":
+                case ".txt":
+                case ".zip":
+                case ".rar":
+                case ".gitattributes":
+                case ".gitignore":
+                case ".md":
+                case ".ds_store":
+                case ".bz2":
+                case ".dll":
+                case ".exe":
+                    return true;
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string Path = System.IO.Directory.GetCurrentDirectory();
+            string Target = System.IO.Directory.GetCurrentDirectory() + "\\FastDL";
+
+            if (System.IO.Directory.Exists(Target))
+                System.IO.Directory.Delete(Target, true);
+
+            FileManager FM = new FileManager(Path);
+            string NewFile;
+            int Depth;
+            int cnt = 0;
+
+            foreach(File file in FM.Files)
+            {
+                if (Ignore(file.Ext) || !file.IsWithinDirectory("addons"))
+                    continue;
+
+                NewFile = "";
+                Depth = 0;
+
+                foreach (string folder in file.Path)
+                {
+                    if (Depth == 0 && folder.ToLower() == "addons")
+                    {
+                        Depth++;
+                        continue;
+                    }
+                    else if (Depth == 1)
+                    {
+                        Depth++;
+                    }
+                    else if (Depth==2)
+                    {
+                        NewFile += folder+"/";
+                    }
+                }
+
+                Console.Out.WriteLine(Target + NewFile + file.Name + file.Ext + " -> " + file.GetFullPath());
+                FM.Copy(Target + "\\" + NewFile + file.Name + file.Ext, file.GetFullPath());
+                FM.Compress(Target + "\\" + NewFile + file.Name + file.Ext);
+                cnt++;
+            }
+
+            System.Console.Out.WriteLine("\nDONE! "+cnt+" files copied and compressed!");
+            System.Console.In.ReadLine();
+        }
+    }
+}
